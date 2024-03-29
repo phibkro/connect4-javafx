63ed602d84da08dfc435c72e6240c48a9b2dad1e
--global <philipbk@stud.ntnu.no>
2024-03-29 02:31:25 +0100

    feat(tictactoe): :sparkles: add basic App and Controller

---
+++ src/main/java/tictactoe/App.java
@@ -0,0 +1,27 @@
+package tictactoe;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  public static void main(String[] args) {
+    Application.launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws IOException {
+    String appPath = "app.fxml";
+    URL appLocation = getClass().getResource(appPath);
+    Scene scene = new Scene(FXMLLoader.load(appLocation));
+
+    primaryStage.setTitle("Tic-Tac-Toe");
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
+}
---
+++ src/main/java/tictactoe/Controller.java
@@ -0,0 +1,58 @@
+package tictactoe;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventTarget;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
+
+public class Controller {
+  @FXML
+  private FlowPane board;
+  @FXML
+  private Text output;
+  @FXML
+  private TextField input;
+
+  @FXML
+  private void initialize() {
+    int sideSize = 3;
+    int boardSize = sideSize * sideSize; // 9
+
+    Button[] tiles = new Button[boardSize];
+    for (int i = 0; i < tiles.length; i++) {
+      Button tile = new Button();
+      tiles[i] = tile;
+
+      tile.setText("[   ]");
+      tile.setPrefSize(
+          board.getPrefWidth() / sideSize,
+          board.getPrefWidth() / sideSize);
+      tile.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+          fireClick(tile);
+        }
+      });
+    }
+    this.board.getChildren().setAll(tiles);
+  }
+
+  void fireClick(Button tile) {
+    tile.setText("null");
+  }
+
+  @FXML
+  void handleSubmit(ActionEvent event) {
+    this.output.setText(input.getText());
+  }
+
+  @FXML
+  void handleTilePress(ActionEvent event) {
+    EventTarget target = event.getTarget();
+    this.output.setText(target.toString());
+  }
+}
