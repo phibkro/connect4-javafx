3dde8c0d76f608409ee7b92dddb6c59abe553b90
--global <philipbk@stud.ntnu.no>
2024-04-09 22:04:17 +0200

    feat(connect4): :sparkles: add game board to GUI

--- src/main/java/connect4/Controller.java
+++ src/main/java/connect4/Controller.java
@@ -5,49 +5,106 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import connect4.models.Game;
+import connect4.models.Tile;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.shape.Circle;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 
 public class Controller implements Initializable {
   private static final int TILE_SIZE = 100;
   private Game game;
-  @FXML
-  private Pane board;
+
+  // Menu elements
   @FXML
   private Button btnStartGame;
   @FXML
   private Button btnLoadGame;
 
+  // Game elements
+  @FXML
+  private HBox board;
+
+  private Pane[] columns = new Pane[Game.COLUMN_COUNT];
+
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     this.game = new Game();
 
-    // Create game view
+    // Create and bind board
     {
-      // Create game Buttons programmatically
-      Button[] tiles = new Button[this.game.getBoardSize()];
-      for (int i = 0; i < tiles.length; i++) {
-        Button tile = new Button();
-        tiles[i] = tile;
+      // Recontextualize attributes with relevant names
 
-        tile.setText("[   ]");
-        tile.setPrefSize(TILE_SIZE, TILE_SIZE);
-        tile.setId(String.format("tile%d", i));
+      // Bind gridShape
+      this.board.getChildren().clear();
 
-        tile.setOnAction(new EventHandler<ActionEvent>() {
-          @Override
-          public void handle(ActionEvent event) {
-            System.out.println("hello");
-          }
-        });
+      // Create gridShape
+      {
+        final double COLUMN_WIDTH = TILE_SIZE;
+        final double COLUMN_HEIGHT = TILE_SIZE * Game.COLUMN_SIZE;
+        for (int i = 0; i < Game.COLUMN_COUNT; i++) {
+          StackPane stackPane = new StackPane();
+          stackPane.setPrefWidth(COLUMN_WIDTH);
+          stackPane.setPrefHeight(COLUMN_HEIGHT);
+
+          VBox vBox = new VBox();
+          vBox.setPrefWidth(COLUMN_WIDTH);
+          vBox.setPrefHeight(COLUMN_HEIGHT);
+          vBox.setRotate(180);
+
+          Button btn = new Button();
+          btn.setPrefWidth(COLUMN_WIDTH);
+          btn.setPrefHeight(COLUMN_HEIGHT);
+          btn.setOpacity(0.50);
+          btn.setDefaultButton(true);
+
+          this.board.getChildren().add(stackPane);
+          stackPane.getChildren().addAll(vBox, btn);
+          final int x = i;
+          this.columns[x] = vBox;
+
+          btn.setOnAction(e -> {
+            if (this.game.isMoveLegal(x)) {
+              final Tile currentPlayer = this.game.getCurrentPlayer();
+              // Update game
+              this.game.makeMove(x);
+
+              // Update ui
+              Circle circle = new Circle(TILE_SIZE / 2);
+              switch (currentPlayer) {
+                case Player:
+                  circle.setFill(Color.PURPLE);
+                  break;
+                case Opponent:
+                  circle.setFill(Color.ORANGE);
+                  break;
+                default:
+                  System.err.println("This shouldn't happen");
+                  break;
+              }
+              this.columns[x].getChildren().add(circle);
+
+              // Update handlers
+              if (!this.game.isMoveLegal(x)) {
+                btn.setDisable(true);
+                btn.setOnAction(null);
+              }
+            } else {
+              System.err.println("wtf");
+            }
+          });
+        }
       }
-      this.board.getChildren().setAll(tiles);
+
     }
+
   }
 
   @FXML
