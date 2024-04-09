0052974dd5aed6efd2318429b0cf1ab1205466ad
--global <philipbk@stud.ntnu.no>
2024-04-09 04:42:15 +0200

    refactor(connect4): :recycle: rename BOARD attributes to COLUMN_COUNT & COLUMN_SIZE
    Previous names implied style. New names imply form.

--- src/main/java/connect4/models/Game.java
+++ src/main/java/connect4/models/Game.java
@@ -3,10 +3,10 @@ package connect4.models;
 import java.util.Arrays;
 
 public class Game implements VectorGame {
-  public static final int BOARD_WIDTH = 7;
-  public static final int BOARD_HEIGHT = 6;
-  public static final int BOARD_SIZE = BOARD_WIDTH * BOARD_HEIGHT;
-  private Tile[] board = new Tile[BOARD_WIDTH * BOARD_HEIGHT];
+  public static final int COLUMN_COUNT = 7;
+  public static final int COLUMN_SIZE = 6;
+  public static final int BOARD_SIZE = COLUMN_COUNT * COLUMN_SIZE;
+  private Tile[] board = new Tile[COLUMN_COUNT * COLUMN_SIZE];
   private Tile currentPlayer;
 
   public Game() {
@@ -19,7 +19,7 @@ public class Game implements VectorGame {
   }
 
   public boolean isValidMove(int column) {
-    BoardHelper boardHelper = new BoardHelper(BOARD_WIDTH, BOARD_HEIGHT, this.board);
+    BoardHelper boardHelper = new BoardHelper(COLUMN_COUNT, COLUMN_SIZE, this.board);
     Tile[] highestRow = boardHelper.getRow(0);
 
     return highestRow[column] == Tile.Empty;
@@ -30,10 +30,10 @@ public class Game implements VectorGame {
       throw new IllegalArgumentException("Invalid move to column " + column);
     }
 
-    BoardHelper boardHelper = new BoardHelper(BOARD_WIDTH, BOARD_HEIGHT, this.board);
+    BoardHelper boardHelper = new BoardHelper(COLUMN_COUNT, COLUMN_SIZE, this.board);
     Tile[] selectedColumn = boardHelper.getColumn(column);
 
-    for (int row = BOARD_HEIGHT - 1; row >= 0; row--) {
+    for (int row = COLUMN_SIZE - 1; row >= 0; row--) {
       if (selectedColumn[row] == Tile.Empty) {
         this.board[boardHelper.translate(row, column)] = this.currentPlayer;
         break;
