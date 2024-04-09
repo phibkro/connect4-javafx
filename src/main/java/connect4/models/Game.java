fa8f4aaeb8bc4b8d1895c67c80494eb06032a484
brage-andreas <draaangooo@gmail.com>
2024-04-10 00:03:39 +0200

    feat: refactor, implement `isGameOver`, add `getWinner`

--- src/main/java/connect4/models/Game.java
+++ src/main/java/connect4/models/Game.java
@@ -1,12 +1,12 @@
 package connect4.models;
 
+import java.io.File;
 import java.util.Arrays;
 
 public class Game implements VectorGame {
-  public static final int COLUMN_COUNT = 7;
-  public static final int COLUMN_SIZE = 6;
-  public static final int BOARD_SIZE = COLUMN_COUNT * COLUMN_SIZE;
-  private Tile[] board = new Tile[COLUMN_COUNT * COLUMN_SIZE];
+  public static final int HEIGHT = 6;
+  public static final int WIDTH = 7;
+  private Tile[] board = new Tile[WIDTH * HEIGHT];
   private Tile currentPlayer = Tile.Player;
 
   public Game() {
@@ -18,8 +18,12 @@ public class Game implements VectorGame {
   }
 
   @Override
-  public boolean isMoveLegal(int column) {
-    BoardHelper boardHelper = new BoardHelper(COLUMN_SIZE, COLUMN_COUNT, this.board);
+  public boolean isLegalMove(int column) {
+    if (column < 0 || column >= Game.WIDTH) {
+      return false;
+    }
+
+    BoardHelper boardHelper = new BoardHelper(Game.HEIGHT, Game.WIDTH, this.board);
     Tile[] highestRow = boardHelper.getRow(0);
 
     return highestRow[column] == Tile.Empty;
@@ -27,24 +31,74 @@ public class Game implements VectorGame {
 
   @Override
   public void makeMove(int column) throws IllegalArgumentException {
-    if (!this.isMoveLegal(column)) {
-      throw new IllegalArgumentException("Invalid move to column " + column);
+    if (!this.isLegalMove(column)) {
+      throw new IllegalArgumentException("Illegal or invalid move to column " + column);
     }
 
-    BoardHelper boardHelper = new BoardHelper(COLUMN_SIZE, COLUMN_COUNT, this.board);
+    BoardHelper boardHelper = new BoardHelper(Game.HEIGHT, Game.WIDTH, this.board);
     Tile[] selectedColumn = boardHelper.getColumn(column);
 
-    for (int row = COLUMN_SIZE - 1; row >= 0; row--) {
+    for (int row = Game.HEIGHT - 1; row >= 0; row--) {
       if (selectedColumn[row] == Tile.Empty) {
         this.board[boardHelper.translate(row, column)] = this.currentPlayer;
         break;
       }
     }
+
+    this.currentPlayer = this.currentPlayer == Tile.Player ? Tile.Opponent : Tile.Player;
   }
 
   @Override
   public boolean isGameOver() {
-    // TODO Auto-generated method stub
+    BoardHelper boardHelper = new BoardHelper(Game.HEIGHT, Game.WIDTH, this.board);
+
+    for (int row = 0; row < Game.HEIGHT; row++) {
+      for (int column = 0; column < Game.WIDTH; column++) {
+        Tile tile = boardHelper.getTile(row, column);
+
+        if (tile == Tile.Empty) {
+          continue;
+        }
+
+        if (boardHelper.isXSelfSameTilesInARowHorizontalOrDiagonal(tile, row, column, 3)) {
+          return true;
+        }
+      }
+    }
+
     return false;
+  }
+
+  @Override
+  public Tile getWinner() {
+    BoardHelper boardHelper = new BoardHelper(Game.HEIGHT, Game.WIDTH, this.board);
+
+    for (int row = 0; row < Game.HEIGHT; row++) {
+      for (int column = 0; column < Game.WIDTH; column++) {
+        Tile tile = boardHelper.getTile(row, column);
+
+        if (tile == Tile.Empty) {
+          continue;
+        }
+
+        if (boardHelper.isXSelfSameTilesInARowHorizontalOrDiagonal(tile, row, column, 3)) {
+          return tile;
+        }
+      }
+    }
+
+    throw new IllegalStateException("No winner found");
+  }
+
+  @Override
+  public void loadGame(File file) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'loadGame'");
+  }
+
+  @Override
+  public File saveGame() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'saveGame'");
   }
 }
